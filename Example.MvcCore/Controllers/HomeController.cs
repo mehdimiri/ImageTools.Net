0dc6623bbb7886d4f49cbbc9d97cac24c0e4182f
Mehdi <mehdikarma.pnu@gmail.com>
2021-11-29 08:31:01 +0330

    First Commit

---
+++ Example.MvcCore/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Example.MvcCore.Models;
+using ImageTools.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.MvcCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            foreach (var file in Request.Form.Files)
+            {
+                if (file.Length > 0)
+                {
+                    using (var stream = file.OpenReadStream())
+                    {
+                        stream.SaveAs("D://");
+                    }
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ImageTools.Net/Save.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageTools.Net
+{
+    public static class Save
+    {
+        public static void SaveAs(this Stream stream, string path)
+        {
+            using (stream)
+            {
+                using (Image img = Image.FromStream(stream))
+                {
+                    //img.Save();
+                }
+            }
+        }
+
+        public static void SaveAs(this Image img, string path)
+        {
+            using (img)
+            {
+                img.SaveAs();
+            }
+        }
+    }
+}
